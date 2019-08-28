8aca9c4186c7bb8ba726fd5c733002aedae5f2fb
harshit-kulshreshtha <46815853+harshit-kulshreshtha@users.noreply.github.com>
2019-08-28 22:47:44 +0530

    Update numberOnly.h

--- data/numberOnly.h
+++ data/numberOnly.h
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <sstream>
+#include <math.h>
 
 using namespace std;
 
 void genPass(char* destination_buffer)	//Desinaion buffer stores the location where the password is to tried
-{	for(int i=0;i<999999999;i++)
+{	int ctr=1;
+	int power=1;	
+	for(int i=0;i<1004;i++)
 	{	ostringstream s;
 		s<<i;
 		string temp=s.str();
+		if(ctr-1==pow(10,power))
+		{	power++;
+			for(int i=0;i<power;i++)
+				destination_buffer[i]='0';
+			cout<<destination_buffer<<endl;
+			i--;
+			continue;
+			ctr++;
+		}		
 		for(int i=0;i<temp.length();i++)
 			destination_buffer[i]=temp.at(i);
 		cout<<destination_buffer<<endl;
+		ctr++;
 	}
 }
 int main()
