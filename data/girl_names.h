05135ab81eb58a1ef1c8ecdea82a0e68485b0c8e
harshit-kulshreshtha <46815853+harshit-kulshreshtha@users.noreply.github.com>
2019-08-29 23:43:36 +0530

    Create girl_names.h

---
+++ data/girl_names.h
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import os
+#print("hello")
+fin=open("/root/girl_names.txt","r");
+fout=open("Indian_name_girl.txt","a");
+lines=fin.readlines()
+for line in lines:
+	lines=line.strip();
+	split_line=lines.split("=")
+	line1=split_line[0]
+	print(line1);	
+	fout.write(line1);
+	fout.write("\n");			
+fin.close();
+fout.close();
+
+
